a0e46e888abd1a69e7ceb9a35535424253c33b46
Vinicius Lima <vinicius-lima@hadesconsulting.com.br>
2025-05-19 20:54:19 -0300

    Alteração na checagem da versão para também aceitar o Arch puro, não apaenas Manjaro

--- install/check-version.sh
+++ install/check-version.sh
@@ -8,10 +8,10 @@ fi
 
 . /etc/os-release
 
-if [ "$ID" != "manjaro" ]; then
+if [ "$ID" != "manjaro" && "$ID" != "arch"  ]; then
   echo "$(tput setaf 1)Error: OS requirement not met"
   echo "You are currently running: $ID"
-  echo "OS required: Manjaro GNOME 24 or higher"
+  echo "OS required: Manjaro GNOME 24 or higher, or Arch Linux"
   echo "Installation stopped."
   exit 1
 fi
